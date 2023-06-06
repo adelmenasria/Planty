1959b74c539b13d2157d4f4f5b72de6d9a3f07d4
Adel Menasria <menasria.adel@outlook.com>
2023-06-07 01:03:26 +0200

    Increment buttons for order page with JS
    Mise à jour du functions.php du thème enfant afin de charger un fichier js dans la page "commander" et boutons de type incrément.

--- wp-content/themes/blankslate-child/functions.php
+++ wp-content/themes/blankslate-child/functions.php
@@ -1,6 +1,21 @@
 <?php
-/** inherit the styles from the parent theme **/
-function theme_enqueue_styles() {
- wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+// inherit the styles from the parent theme
+add_action( 'wp_enqueue_scripts', 'enqueue_custom_scripts' );
+function enqueue_custom_scripts() {
+    // Chargement du fichier CSS personnalisé
+    wp_enqueue_style( 'child-custom-styles', get_stylesheet_directory_uri() . '/style.css' );
+
+    // Vérifier si c'est la page spécifique sur laquelle vous souhaitez charger le fichier JavaScript
+    if ( is_page( 'order' ) ) {
+        wp_enqueue_script( 'child-custom-scripts', get_stylesheet_directory_uri() . '/custom-script.js', array( 'jquery' ), '1.0', true );
+    }
 }
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+
+/** Disable p and span tags (cf7) **/
+add_filter('wpcf7_autop_or_not', '__return_false');
+
+add_filter('wpcf7_form_elements', function($content) {
+    $content = preg_replace('/<(span).*?class="\s*(?:.*\s)?wpcf7-form-control-wrap(?:\s[^"]+)?\s*"[^\>]*>(.*)<\/\1>/i', '\2', $content);
+    return $content;
+});
