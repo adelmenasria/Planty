332925dd46a029b1ef69ddec7d8909fc607044cb
Adel Menasria <menasria.adel@outlook.com>
2023-06-09 08:07:03 +0200

    Update comments functions.php

--- wp-content/themes/blankslate-child/functions.php
+++ wp-content/themes/blankslate-child/functions.php
@@ -4,7 +4,7 @@ add_action('wp_enqueue_scripts', 'enqueue_custom_scripts');
 function enqueue_custom_scripts() {
     // CSS
     wp_enqueue_style('child-custom-styles', get_stylesheet_directory_uri() . '/style.css');
-    // JS file if page order
+    // Load a JS file on order page only
     if (is_page('order')) {
         wp_enqueue_script('child-custom-scripts', get_stylesheet_directory_uri() . '/my-script.js', array('jquery'), '1.0', true);
     }
@@ -15,10 +15,10 @@ function enqueue_custom_scripts() {
 add_filter('wp_nav_menu_args', 'display_navs');
 function display_navs($args = ''){
     if ( is_user_logged_in() ) {
-        // Logged in menu to display
+        // Show logged-in menu
         $args['menu'] = 4;
     } else {
-        // Non-logged-in menu to display
+        // Show logged-out menu
         $args['menu'] = 5;
     }
     return $args;
